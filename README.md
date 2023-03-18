fa2554ec394f2fc940248429a4f77e0d3148bb1c
Dan Maxim <dmaxim@mxinformatics.com>
2023-03-18 15:04:53 -0400

    Update config and readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Mxinfo.Configuration.App
 
 Configuration management application.
+
+
+
+## Azure Ad Configuration
+
+The Terraform in the query and management api project will setup the required Azure AD applications that are utilized by the application.
+
+NOTE:
+
+If there are errors posting to the token endpoint when making calls to the query / management API verify the api app registration has added this applications client in the "Add a client section"
--- config-app/public/config.js
+++ config-app/public/config.js
@@ -1,5 +1,5 @@
-window.BASE_QUERY_API_URL = 'http://apam-config-query.maxdevsec.com/api';
+window.BASE_QUERY_API_URL = 'https://apam-config-query.maxdevsec.com/api';
 window.BASE_MANAGEMENT_API_URL = 'https://apam-config-mgt.maxdevsec.com/api';
 window.CLIENT_ID = '9d484e18-382e-4574-86d3-d29a29907b9c';
-window.AUTHORITY = 'https://login.microsoftonline.com/38ec304a-0fad-408b-b7cd-e04a2e25b885';
-window.REDIRECT_URI = 'http://localhost:3000';
+window.AUTHORITY =  'https://login.microsoftonline.com/38ec304a-0fad-408b-b7cd-e04a2e25b885';
+window.REDIRECT_URI = 'https://apam-config-app.maxdevsec.com';
